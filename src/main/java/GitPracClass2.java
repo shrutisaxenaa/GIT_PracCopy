b8639a9f7bb4a528c47a9ddabde1ba0beaa1af0b
shrutisaxenaa <106474924+shrutisaxenaa@users.noreply.github.com>
2022-07-03 18:30:57 -0500

    committed

--- src/main/java/GitPracClass2.java
+++ src/main/java/GitPracClass2.java
@@ -4,11 +4,11 @@ public class GitPracClass2 {
 	public static void main(String[] args) {
 		System.out.println("my name is shruti");
 		
-		sumNumbers(4,9);
+		sumNumbers(4,9,1);
 	}
 	
-	public static int sumNumbers(int a,int b) {
-		int sum=a+b;
+	public static int sumNumbers(int a,int b,int c) {
+		int sum=a+b+c;
 		System.out.println(sum);
 		return sum;
 	}
